69bff67fc5cdf764bd094b59a81e9e716980de9b
Jonas Krook <jonas.krook@zenuity.com>
2020-12-03 07:54:48 +0100

    Solving task 2 in main

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -3,4 +3,7 @@ use day_02;
 fn main() {
     let num_valid = day_02::count_valid_entries("data/passwords.txt");
     println!("Nr of valid passwords: {}", num_valid);
+
+    let num_new_valid = day_02::count_new_valid_entries("data/passwords.txt");
+    println!("Nr of valid passwords with new rules: {}", num_new_valid);
 }
