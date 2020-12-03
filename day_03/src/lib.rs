4c08402c1cab271e3c309943fb67d3302ef16831
Jonas Krook <jonas.krook@zenuity.com>
2020-12-03 08:06:05 +0100

    Counting the trees and testing

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -1,7 +1,25 @@
+use std::fs;
+
+pub fn count_trees(filename: &str) -> u32 {
+    let forest = fs::read_to_string(filename).unwrap();
+    let mut column = 0;
+    let mut count = 0;
+    for line in forest.lines() {
+        let pos = &line[column..column+1];
+        if pos == "#" {
+            count += 1;
+        }
+        column = (column + 3) % line.len();
+    }
+    count
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::count_trees;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_example() {
+        assert_eq!(7, count_trees("data/example.txt"));
     }
 }
