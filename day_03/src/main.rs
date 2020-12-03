310e9819f3531cbb05f5ae519a7303a2939a7675
Jonas Krook <jonas.krook@zenuity.com>
2020-12-03 08:31:35 +0100

    Printing solution in main

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -3,4 +3,7 @@ use day_03;
 fn main() {
     let count = day_03::count_trees("data/woods.txt");
     println!("Number of trees in path: {}", count);
+
+    let product = day_03::multiply_trees("data/woods.txt");
+    println!("Product of trees in paths: {}", product);
 }
