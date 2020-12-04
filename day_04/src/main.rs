5b11cddbf8c6ce2206246790299d95a89692e323
Jonas Krook <jonas.krook@zenuity.com>
2020-12-04 10:21:29 +0100

    Printing solution to second task in main

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     let valid_count = day_04::nr_valid_passports("data/passports.txt");
     println!("Nr of valid passports: {}", valid_count);
+
+    let strict_valid_count = day_04::nr_strict_valid_passports("data/passports.txt");
+    println!("Nr of strict valid passports: {}", strict_valid_count);
 }
