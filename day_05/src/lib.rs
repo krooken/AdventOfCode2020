09b8c9ac58fb477764d0e9cdc0493dd30b540051
Jonas Krook <jonas.krook@zenseact.com>
2020-12-05 21:56:06 +0100

    Running fmt on project

--- day_05/src/lib.rs
+++ day_05/src/lib.rs
@@ -9,7 +9,7 @@ struct BoardingPass {
 
 impl BoardingPass {
     fn new(row: String, column: String) -> BoardingPass {
-        BoardingPass{
+        BoardingPass {
             row,
             column,
             row_nr: None,
@@ -29,43 +29,46 @@ fn read_boarding_pass(text: &str) -> BoardingPass {
 }
 
 fn parse_boarding_pass(pass: &mut BoardingPass) {
-    let row_nr = generic_binary_parse(&pass.row, |byte| {
-        match byte {
-            b'F' => 0,
-            b'B' => 1,
-            _ => panic!(),
-        }
+    let row_nr = generic_binary_parse(&pass.row, |byte| match byte {
+        b'F' => 0,
+        b'B' => 1,
+        _ => panic!(),
     });
     pass.row_nr = Some(row_nr);
-    let column_nr = generic_binary_parse(&pass.column, |byte| {
-        match byte {
-            b'L' => 0,
-            b'R' => 1,
-            _ => panic!(),
-        }
+    let column_nr = generic_binary_parse(&pass.column, |byte| match byte {
+        b'L' => 0,
+        b'R' => 1,
+        _ => panic!(),
     });
     pass.column_nr = Some(column_nr);
 }
 
-fn generic_binary_parse<F>(text: &str, f: F) -> u32 where F: Fn(&u8) -> u32 {
-    text.as_bytes().iter().rev().enumerate().fold(0, |acc, (i, byte)| {
-        acc + f(byte) * 2u32.pow(i as u32)
-    })
+fn generic_binary_parse<F>(text: &str, f: F) -> u32
+where
+    F: Fn(&u8) -> u32,
+{
+    text.as_bytes()
+        .iter()
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (i, byte)| acc + f(byte) * 2u32.pow(i as u32))
 }
 
 fn get_max(text: &str) -> u32 {
-    text.lines().map(|row| {
-        let mut pass = read_boarding_pass(row);
-        parse_boarding_pass(&mut pass);
-        pass
-    }).fold(0, |acc, pass| {
-        let id = pass.id();
-        if id > acc {
-            id
-        } else {
-            acc
-        }
-    })
+    text.lines()
+        .map(|row| {
+            let mut pass = read_boarding_pass(row);
+            parse_boarding_pass(&mut pass);
+            pass
+        })
+        .fold(0, |acc, pass| {
+            let id = pass.id();
+            if id > acc {
+                id
+            } else {
+                acc
+            }
+        })
 }
 
 pub fn get_max_pass_id(filename: &str) -> u32 {
@@ -75,11 +78,14 @@ pub fn get_max_pass_id(filename: &str) -> u32 {
 
 pub fn get_missing_seat(filename: &str) -> u32 {
     let text = fs::read_to_string(filename).unwrap();
-    let mut sorted_passes = text.lines().map(|row| {
-        let mut pass = read_boarding_pass(row);
-        parse_boarding_pass(&mut pass);
-        pass.id()
-    }).collect::<Vec<u32>>();
+    let mut sorted_passes = text
+        .lines()
+        .map(|row| {
+            let mut pass = read_boarding_pass(row);
+            parse_boarding_pass(&mut pass);
+            pass.id()
+        })
+        .collect::<Vec<u32>>();
     sorted_passes.sort();
     let mut last_id = *sorted_passes.first().unwrap();
     let mut missing_id = 0;
@@ -95,7 +101,7 @@ pub fn get_missing_seat(filename: &str) -> u32 {
 
 #[cfg(test)]
 mod tests {
-    use crate::{read_boarding_pass, parse_boarding_pass, get_max_pass_id};
+    use crate::{get_max_pass_id, parse_boarding_pass, read_boarding_pass};
 
     #[test]
     fn test_split() {
