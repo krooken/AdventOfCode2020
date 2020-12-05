5ed69bdd22e4251e82bb76a151204ea7a275181b
Jonas Krook <jonas.krook@zenseact.com>
2020-12-05 17:12:16 +0100

    Adding function for splitting boarding pass text into row and column information

--- day_05/src/lib.rs
+++ day_05/src/lib.rs
@@ -1,7 +1,34 @@
+struct BoardingPass {
+    row: String,
+    column: String,
+    row_nr: Option<u32>,
+    column_nr: Option<u32>,
+}
+
+impl BoardingPass {
+    fn new(row: String, column: String) -> BoardingPass {
+        BoardingPass{
+            row,
+            column,
+            row_nr: None,
+            column_nr: None,
+        }
+    }
+}
+
+fn read_boarding_pass(text: &str) -> BoardingPass {
+    BoardingPass::new(text[..7].to_string(), text[7..].to_string())
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::read_boarding_pass;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_split() {
+        let text = "FBFBBFFRLR";
+        let pass = read_boarding_pass(text);
+        assert_eq!("FBFBBFF".to_string(), pass.row);
+        assert_eq!("RLR".to_string(), pass.column);
     }
 }
