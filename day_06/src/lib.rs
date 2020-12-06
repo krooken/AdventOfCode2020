844d954d3dca58e04699cd1d51f09bdbaaab804b
Jonas Krook <jonas.krook@zenseact.com>
2020-12-06 07:14:57 +0100

    Counting the flight numbers and printing them in main

--- day_06/src/lib.rs
+++ day_06/src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::fs;
 
 fn get_group_answers(text: &str) -> HashSet<String> {
     let re = regex::Regex::new(r"[a-z]").unwrap();
@@ -14,9 +15,14 @@ fn get_flight_answers(text: &str) -> Vec<HashSet<String>> {
     re.split(text).map(|group| get_group_answers(group)).collect()
 }
 
+pub fn count_flight_answers(filename: &str) -> u32 {
+    let text = fs::read_to_string(filename).unwrap();
+    get_flight_answers(&text).iter().fold(0, |acc, set| acc + set.len() as u32)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{get_group_answers, get_flight_answers};
+    use crate::{get_group_answers, get_flight_answers, count_flight_answers};
     use std::collections::HashSet;
     use std::fs;
 
@@ -42,5 +48,10 @@ mod tests {
         assert_eq!(3, flight[2].len());
         assert_eq!(1, flight[3].len());
         assert_eq!(1, flight[4].len());
+    }
+
+    #[test]
+    fn test_get_flight_count() {
+        assert_eq!(11, count_flight_answers("data/example.txt"));
     }
 }
---
+++ day_06/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let count = day_06::count_flight_answers("data/answers.txt");
+    println!("Number of answers for flight: {}", count);
+}
