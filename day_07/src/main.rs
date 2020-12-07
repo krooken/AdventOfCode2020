a785a612ed6fd31f6a0d3cf59452c94ad9f9f0f1
Jonas Krook <jonas.krook@zenseact.com>
2020-12-07 23:31:05 +0100

    Printing solution for task 1 in main

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("{}", 1);
+    let num = day_07::count_bags_carrying_bag("data/bag_rules.txt", "shiny gold");
+    println!("Number of colors which can carry the bag: {}", num);
 }
