2ab5d3b42716e89298a18ab681da69f790705deb
Jonas Krook <jonas.krook@zenseact.com>
2020-12-08 15:09:39 +0100

    Printing solution to task 2 in main

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     let acc = day_08::get_acc_of_program("data/boot_code.txt");
     println!("Acc at loop: {}", acc);
+
+    let acc = day_08::get_acc_at_end("data/boot_code.txt");
+    println!("Acc at end of changed program: {}", acc);
 }
