27eb8fc8e624705737a68d423bda1f5f18804f41
Jonas Krook <jonas.krook@zenseact.com>
2020-12-08 08:44:32 +0100

    Finding loop of program

--- day_08/src/lib.rs
+++ day_08/src/lib.rs
@@ -1,3 +1,4 @@
+use Instruction::*;
 
 #[derive(Debug, PartialEq)]
 enum Instruction {
@@ -16,9 +17,9 @@ fn get_instruction(line: &str) -> Instruction {
     let cap = re.captures(line).unwrap();
     let num = cap[2].parse().unwrap();
     match &cap[1] {
-        "acc" => Instruction::Acc(num),
-        "jmp" => Instruction::Jmp(num),
-        "nop" => Instruction::Noop(num),
+        "acc" => Acc(num),
+        "jmp" => Jmp(num),
+        "nop" => Noop(num),
         _ => panic!(),
     }
 }
@@ -33,9 +34,33 @@ fn get_program(program: &str) -> Vec<CodeLine> {
     }).collect()
 }
 
+pub fn acc_at_loop(program: &str) -> i32 {
+    let mut code = get_program(program);
+    let mut acc = 0;
+    let mut next_instruction: Option<usize> = Some(0);
+    while next_instruction.is_some() {
+        let index = next_instruction.unwrap();
+        let mut current_code = code.get_mut(index).unwrap();
+        if current_code.visited {
+            next_instruction = None;
+        } else {
+            match current_code.instruction {
+                Acc(num) => {
+                    acc += num;
+                    next_instruction = Some(index + 1);
+                },
+                Jmp(num) => next_instruction = Some((index as i32 + num) as usize),
+                Noop(_) => next_instruction = Some(index + 1),
+            };
+            current_code.visited = true;
+        }
+    }
+    acc
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{get_instruction, get_program};
+    use crate::{get_instruction, get_program, acc_at_loop};
     use crate::Instruction::{Acc, Jmp, Noop};
     use std::fs;
 
@@ -60,5 +85,11 @@ mod tests {
         assert_eq!(Noop(0), get_program(&program)[0].instruction);
         assert_eq!(Acc(6), get_program(&program)[8].instruction);
         assert!(!get_program(&program)[0].visited);
+    }
+
+    #[test]
+    fn test_acc_at_loop() {
+        let program = fs::read_to_string("data/example.txt").unwrap();
+        assert_eq!(5, acc_at_loop(&program));
     }
 }
