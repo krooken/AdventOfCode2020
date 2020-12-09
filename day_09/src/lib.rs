e2c5b942a087d88a8affa2d78e56e3c19bd270c2
Jonas Krook <jonas.krook@zenseact.com>
2020-12-09 07:19:13 +0100

    Checking validity of next number

--- day_09/src/lib.rs
+++ day_09/src/lib.rs
@@ -1,7 +1,30 @@
+fn is_valid(preamble: &[u32], next: u32) -> bool {
+    let mut result = false;
+    for i in 0..preamble.len() {
+        for j in i+1..preamble.len() {
+            if preamble[i] + preamble[j] == next {
+                result = true;
+                break;
+            }
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
+    use std::fs;
+    use crate::is_valid;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_is_valid1() {
+        let codes: Vec<u32> = fs::read_to_string("data/example.txt").unwrap().lines().map(|e| e.parse().unwrap()).collect();
+        assert!(is_valid(&codes[0..5], codes[5]));
+    }
+
+    #[test]
+    fn test_is_not_valid1() {
+        let codes: Vec<u32> = fs::read_to_string("data/example.txt").unwrap().lines().map(|e| e.parse().unwrap()).collect();
+        assert!(!is_valid(&codes[9..14], codes[14]));
     }
 }
