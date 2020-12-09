40720350c18095b79548989aca8340fbdda3db73
Jonas Krook <jonas.krook@zenseact.com>
2020-12-09 08:47:59 +0100

    Printing sum in main

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let filename = "data/code.txt";
     let row = day_09::find_invalid(filename, 25);
-    println!("First invalid row is: {}", row);
+    println!("First invalid number is: {}", row);
+
+    let sum = day_09::get_min_max_sum(filename, 25);
+    println!("Weakness is: {}", sum);
 }
