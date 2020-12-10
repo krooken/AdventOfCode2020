be0654ab6c30321462cf1d26df6a42740df46f90
Jonas Krook <jonas.krook@zenseact.com>
2020-12-10 08:55:17 +0100

    Calculating nr of arrangements and printing in main

--- day_10/src/lib.rs
+++ day_10/src/lib.rs
@@ -29,12 +29,49 @@ pub fn get_product(filename: &str) -> u32 {
     one * three
 }
 
+fn traverse_arrangements(adapters: &Vec<u32>, current: usize, known: &mut Vec<Option<u64>>) -> u64 {
+    if current == adapters.len() - 1 {
+        1
+    } else {
+        let mut sum = 0;
+        let cur_jolt = adapters[current];
+        for (i, next_jolt) in adapters.iter().enumerate().skip(current + 1).take(3) {
+            let diff = next_jolt - cur_jolt;
+            if diff > 0 && diff < 4 {
+                if let Some(num) = known[i] {
+                    sum += num;
+                } else {
+                    let res = traverse_arrangements(adapters, i, known);
+                    known[i] = Some(res);
+                    sum += res;
+                }
+            }
+        }
+        sum
+    }
+}
+
+pub fn get_arrangements(filename: &str) -> u64 {
+    let mut adapters: Vec<u32> = fs::read_to_string(filename).unwrap().lines().map(|e| e.parse().unwrap()).collect();
+    adapters.push(0);
+    adapters.sort();
+    let i = adapters.len() - 1;
+    adapters.push(adapters[i] + 3);
+    let mut known: Vec<Option<u64>> = (0..adapters.len() as u64).map(|_| None).collect();
+    traverse_arrangements(&adapters, 0, &mut known)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::get_nr_differences;
+    use crate::{get_nr_differences, get_arrangements};
 
     #[test]
     fn test_nr_differences() {
         assert_eq!((22, 0, 10), get_nr_differences("data/example.txt"));
+    }
+
+    #[test]
+    fn test_arrangements() {
+        assert_eq!(19208, get_arrangements("data/example.txt"));
     }
 }
--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -2,4 +2,7 @@ fn main() {
     let filename = "data/adapters.txt";
     let product = day_10::get_product(filename);
     println!("The distribution is: {}", product);
+
+    let arrangements = day_10::get_arrangements(filename);
+    println!("Nr of arrangements: {}", arrangements);
 }
