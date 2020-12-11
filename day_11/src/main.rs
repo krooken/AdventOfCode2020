98c68a0b5c339330cb3d6edcb91e5215c3d31468
Jonas Krook <jonas.krook@zenseact.com>
2020-12-11 08:14:17 +0100

    Printing task 2 solution in main

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -2,4 +2,7 @@ fn main() {
     let filename = "data/seats.txt";
     let occupied = day_11::simulate(filename);
     println!("Nr of occupied at fixed point: {}", occupied);
+
+    let occupied = day_11::simulate_visible(filename);
+    println!("Nr of occupied at fixed point: {}", occupied);
 }
