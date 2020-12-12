cdee064a13a0802ecd36da6e6c3d7d27cdf7221b
Jonas Krook <jonas.krook@zenseact.com>
2020-12-12 19:34:32 +0100

    Printing result of task 2 in main

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -2,4 +2,7 @@ fn main() {
     let filename = "data/directions.txt";
     let distance_to_destination = day_12::sail_to_destination(filename);
     println!("Distance to destination: {}", distance_to_destination);
+
+    let distance_with_waypoints = day_12::sail_with_waypoint(filename);
+    println!("Distance with waypoints: {}", distance_with_waypoints);
 }
