fe465b5e1acf191c35f922d19ffe0179c65d6567
Jonas Krook <jonas.krook@zenseact.com>
2020-12-12 09:07:16 +0100

    Commands and movement for ship

--- day_12/src/lib.rs
+++ day_12/src/lib.rs
@@ -23,11 +23,68 @@ impl Position {
     fn manhattan(&self) -> i64 {
         self.east.abs() + self.north.abs()
     }
+
+    fn left(&mut self) {
+        self.orientation = match &self.orientation {
+            Orientation::East => Orientation::North,
+            Orientation::North => Orientation::West,
+            Orientation::West => Orientation::South,
+            Orientation::South => Orientation::East,
+        }
+    }
+
+    fn right(&mut self) {
+        self.orientation = match &self.orientation {
+            Orientation::East => Orientation::South,
+            Orientation::North => Orientation::East,
+            Orientation::West => Orientation::North,
+            Orientation::South => Orientation::West,
+        }
+    }
+
+    fn sail(&mut self, command: Command) {
+        let translated_command = match command {
+            Command::Forward(num) => {match self.orientation {
+                Orientation::East => Command::East(num),
+                Orientation::North => Command::North(num),
+                Orientation::West => Command::West(num),
+                Orientation::South => Command::South(num),
+            }},
+            _ => command,
+        };
+        match translated_command {
+            Command::East(num) => self.east += num,
+            Command::North(num) => self.north += num,
+            Command::West(num) => self.east -= num,
+            Command::South(num) => self.north -= num,
+            Command::Right(num) => {
+                for _ in 0..num/90 {
+                    self.right();
+                }
+            },
+            Command::Left(num) => {
+                for _ in 0..num/90 {
+                    self.left();
+                }
+            }
+            _ => panic!(),
+        }
+    }
+}
+
+enum Command {
+    East(i64),
+    North(i64),
+    West(i64),
+    South(i64),
+    Forward(i64),
+    Right(i64),
+    Left(i64),
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Position;
+    use crate::{Position, Command};
 
     #[test]
     fn test_manhattan() {
@@ -35,5 +92,16 @@ mod tests {
         pos.north += 10;
         pos.east += -5;
         assert_eq!(15, pos.manhattan());
+    }
+
+    #[test]
+    fn test_sail() {
+        let mut pos = Position::new();
+        pos.sail(Command::Forward(10));
+        pos.sail(Command::North(3));
+        pos.sail(Command::Forward(7));
+        pos.sail(Command::Right(90));
+        pos.sail(Command::Forward(11));
+        assert_eq!(25, pos.manhattan());
     }
 }
