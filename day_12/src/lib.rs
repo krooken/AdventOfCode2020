483358308901e666c928efe615198696bf3b3731
Jonas Krook <jonas.krook@zenseact.com>
2020-12-12 08:40:17 +0100

    Data structures for position and method for manhattan distance

--- day_12/src/lib.rs
+++ day_12/src/lib.rs
@@ -1,7 +1,39 @@
+enum Orientation {
+    East,
+    North,
+    West,
+    South,
+}
+
+struct Position {
+    north: i64,
+    east: i64,
+    orientation: Orientation,
+}
+
+impl Position {
+    fn new() -> Position {
+        Position {
+            north: 0,
+            east: 0,
+            orientation: Orientation::East,
+        }
+    }
+
+    fn manhattan(&self) -> i64 {
+        self.east.abs() + self.north.abs()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::Position;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_manhattan() {
+        let mut pos = Position::new();
+        pos.north += 10;
+        pos.east += -5;
+        assert_eq!(15, pos.manhattan());
     }
 }
