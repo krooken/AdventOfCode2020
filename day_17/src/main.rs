be34075ba4aaeb8876f0a05ab2c4dc4c06c6a13a
Jonas Krook <jonas.krook@zenseact.com>
2020-12-17 17:31:17 +0100

    Printing result for task 2 in main

--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -2,4 +2,7 @@ fn main() {
     let filename = "data/cubes.txt";
     let nr_cubes = day_17::simulate(&filename);
     println!("Nr of active cubes after 6 cycles: {}", nr_cubes);
+
+    let nr_cubes = day_17::simulate4(&filename);
+    println!("Nr of active cubes in 4D after 6 cycles: {}", nr_cubes);
 }
