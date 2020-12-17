6e27beb6084bc62ad1b5134887ed009d1ba4b0c7
Jonas Krook <jonas.krook@zenseact.com>
2020-12-17 08:59:58 +0100

    Parsing grid into coordinates and taking extreme values.

--- day_17/src/lib.rs
+++ day_17/src/lib.rs
@@ -1,7 +1,46 @@
+fn extreme_coordinate<F>(cubes: &Vec<(i64, i64, i64)>, f: F) -> (i64, i64, i64)
+where F: Fn(i64, i64) -> bool {
+    cubes.iter().fold((0, 0, 0), |(x_acc, y_acc, z_acc), (x, y, z)| {
+        let mut m = (x_acc, y_acc, z_acc);
+        if f(*x, x_acc) {
+            m.0 = *x;
+        }
+        if f(*y, y_acc) {
+            m.1 = *y;
+        }
+        if f(*z, z_acc) {
+            m.2 = *z;
+        }
+        m
+    })
+}
+
+fn max_coordinate(cubes: &Vec<(i64, i64, i64)>) -> (i64, i64, i64) {
+    extreme_coordinate(cubes, |v, acc| v > acc)
+}
+
+fn min_coordinate(cubes: &Vec<(i64, i64, i64)>) -> (i64, i64, i64) {
+    extreme_coordinate(cubes, |v, acc| v < acc)
+}
+
+fn get_coordinates(text: &str) -> Vec<(i64, i64, i64)> {
+    text.lines().enumerate().map(|(i, line)| {
+        line.chars().enumerate().filter(|(_, c)| c == &'#').map(move |(j, _)| {
+            (j as i64, i as i64, 0)
+        })
+    }).flatten().collect()
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::{get_coordinates, min_coordinate, max_coordinate};
+    use std::fs;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_min_max() {
+        let text = fs::read_to_string("data/example.txt").unwrap();
+        let coords = get_coordinates(&text);
+        assert_eq!((0, 0, 0), min_coordinate(&coords));
+        assert_eq!((2, 2, 0), max_coordinate(&coords));
     }
 }
