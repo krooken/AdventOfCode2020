fade9042c58102895564ae4fbadb6321b92a0035
Jonas Krook <jonas.krook@zenseact.com>
2021-01-07 16:46:25 +0100

    Calculating how much to wait for a bus

--- day_13/src/lib.rs
+++ day_13/src/lib.rs
@@ -1,7 +1,23 @@
+fn wait_in_minutes(bus_id: u32, departure_time: u32) -> u32 {
+    (bus_id - departure_time % bus_id) % bus_id
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::wait_in_minutes;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_wait_in_minutes_zero() {
+        assert_eq!(0, wait_in_minutes(13, 13));
+    }
+
+    #[test]
+    fn test_wait_in_minutes_1() {
+        assert_eq!(1, wait_in_minutes(13, 12));
+    }
+
+    #[test]
+    fn test_wait_in_minutes_2() {
+        assert_eq!(1, wait_in_minutes(13, 25));
     }
 }
