d77897fa4c070b2e7345f89863a494b90249197d
Jonas Krook <jonas.krook@zenseact.com>
2021-01-07 17:31:17 +0100

    Calculating wait and bus id product

--- day_13/src/lib.rs
+++ day_13/src/lib.rs
@@ -1,17 +1,34 @@
+use std::fs;
+
 fn wait_in_minutes(bus_id: u32, departure_time: u32) -> u32 {
     (bus_id - departure_time % bus_id) % bus_id
 }
 
-fn shortest_wait(timetable: &Vec<u32>, departure_time: u32) -> u32 {
-    timetable.iter().fold(timetable[0], |acc, e| {
+fn shortest_wait(timetable: &Vec<u32>, departure_time: u32) -> (u32, u32) {
+    timetable.iter().fold((timetable[0], timetable[0]), |(acc, id), e| {
         let wait = wait_in_minutes(*e, departure_time);
-        if wait < acc {wait} else {acc}
+        if wait < acc {(wait, *e)} else {(acc, id)}
     })
+}
+
+pub fn calculate_product(filename: &str) -> u32 {
+    let text = fs::read_to_string(filename).unwrap();
+    let mut rows = text.lines();
+    let departure: u32 = rows.next().unwrap().parse().unwrap();
+    let mut timetable: Vec<u32> = Vec::new();
+    for time in rows.next().unwrap().split(",") {
+        match time.parse() {
+            Ok(num) => timetable.push(num),
+            _ => (),
+        }
+    }
+    let (wait, id) = shortest_wait(&timetable, departure);
+    wait * id
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{wait_in_minutes, shortest_wait};
+    use crate::{wait_in_minutes, shortest_wait, calculate_product};
 
     #[test]
     fn test_wait_in_minutes_zero() {
@@ -30,6 +47,11 @@ mod tests {
 
     #[test]
     fn test_earliest_departure() {
-        assert_eq!(5, shortest_wait(&vec![7, 13, 59, 31, 19], 939));
+        assert_eq!(5, shortest_wait(&vec![7, 13, 59, 31, 19], 939).0);
+    }
+
+    #[test]
+    fn test_calculate_product() {
+        assert_eq!(295, calculate_product("data/example.txt"));
     }
 }
