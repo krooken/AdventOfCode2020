b0b0f202ce881909d9516f6cf801cf35f84f8305
Jonas Krook <jonas.krook@zenseact.com>
2021-01-11 13:24:22 +0100

    Prinitng result in main

--- day_13/src/main.rs
+++ day_13/src/main.rs
@@ -2,4 +2,7 @@ fn main() {
     let filename = "data/timetable.txt";
     let product = day_13::calculate_product(filename);
     println!("The wait and bus id product is: {}", product);
+
+    let departure = day_13::calculate_earliest_departure(filename);
+    println!("The earliest departure is: {}", departure);
 }
